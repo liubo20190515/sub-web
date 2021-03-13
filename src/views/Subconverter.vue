249c3f037f3d1b0df8d3b73fdc4794b5ff967975
liubo20190515 <50657503+liubo20190515@users.noreply.github.com>
2021-03-13 19:09:02 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -256,7 +256,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          "subconverter-phi.vercel.app(自动编译最新版本后端-测试）": "https://subconverter-phi.vercel.app/sub?",
+          "my-subconverter.vercel.app(自动编译最新版本后端-测试）": "https://my-subconverter.vercel.app/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
@@ -264,7 +264,7 @@ export default {
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
-          { value: "https://subconverter-phi.vercel.app/sub?" },
+          { value: "https://my-subconverter.vercel.app/sub?" },
           { value: "https://subcon.dlj.tf/sub?" },
           { value: "https://api.dler.io/sub?" },
           { value: "https://api.wcc.best/sub?" },
