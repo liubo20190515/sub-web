dcf9e759167c7237716d155c165b52f69a7950c7
liubo20190515 <50657503+liubo20190515@users.noreply.github.com>
2021-03-13 19:09:43 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -574,7 +574,7 @@ export default {
   },
   mounted() {
     this.form.clientType = "clash";
-    this.form.customBackend = "https://subconverter-phi.vercel.app/sub?";
+    this.form.customBackend = "https://my-subconverter.vercel.app/sub?";
     this.form.remoteConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini";
     this.getBackendVersion();
   },
